f732839a5869be0494d05fc5f850e70efb8bffd3
Killerfrizz <kirill22561@gmail.com>
2024-04-23 22:33:59 +0300

    create project, create player and add physics

---
+++ tanks online/src/App.jsx
@@ -0,0 +1,24 @@
+import { PointerLockControls, Sky } from "@react-three/drei";
+import { Ground } from "./Ground.jsx";
+import { Physics, RigidBody } from "@react-three/rapier";
+import { Player } from "./player.jsx";
+
+export const App = () => {
+  return (
+    <>
+      <PointerLockControls />
+      <Sky sunPosition={[100, 20, 100]}></Sky>
+      <ambientLight intensity={1.5} />
+      <Physics gravity={[0, -20, 0]}>
+        <Ground />
+        <Player/>
+        <RigidBody>
+          <mesh position={[0, 3, -5]}>
+            <boxGeometry />
+          </mesh>
+        </RigidBody>
+      </Physics>
+    </>
+  );
+};
+export default App;
